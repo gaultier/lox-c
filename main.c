096ee09260f9f7ffcbbe9e1fc234a0ed4559949c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-01-06 21:10:11 +0100

    first

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_file(const char path[], char** content, size_t* content_len) {
+    FILE* file = NULL;
+
+    if ((file = fopen(path, "r")) == NULL) {
+        fprintf(stderr, "Could not open the file `%s`: errno=%d error=%s\n",
+                path, errno, strerror(errno));
+        exit(errno);
+    }
+
+    int ret = 0;
+    if ((ret = fseek(file, 0, SEEK_END)) != 0) {
+        fprintf(stderr,
+                "Could not move the file cursor to the end of the file `%s`: "
+                "errno=%d error=%s\n",
+                path, errno, strerror(errno));
+        exit(errno);
+    }
+    const long file_size = ftell(file);
+
+    rewind(file);
+
+    *content = calloc(file_size + 1, 1);
+    if (*content == NULL) {
+        fprintf(stderr, "Could not allocate file content: errno=%d error=%s\n",
+                errno, strerror(errno));
+        exit(errno);
+    }
+
+    const size_t bytes_read = fread(*content, 1, file_size, file);
+    (*content)[bytes_read] = '\0';
+    *content_len = bytes_read;
+
+    fclose(file);
+}
+
+int main(int argc, char* argv[]) {
+    char* content = NULL;
+    size_t content_len = 0;
+    read_file(argv[1], &content, &content_len);
+
+    printf("%s", content);
+}
