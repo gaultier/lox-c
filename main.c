b8b8efa44ad98804f4505bed8de2f84d3dd4836d
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-01-08 15:37:56 +0100

    Add parse rules

--- main.c
+++ main.c
@@ -594,6 +594,62 @@ typedef struct {
     Chunk* chunk;
 } Parser;
 
+typedef void (*ParseFn)(Parser*);
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
+static void parse_grouping(Parser*);
+static void parse_unary(Parser*);
+static void parse_binary(Parser*);
+static void parse_number(Parser*);
+
+static const ParseRule rules[] = {
+    {parse_grouping, NULL, PREC_NONE},       // TOKEN_LEFT_PAREN
+    {NULL, NULL, PREC_NONE},                 // TOKEN_RIGHT_PAREN
+    {NULL, NULL, PREC_NONE},                 // TOKEN_LEFT_BRACE
+    {NULL, NULL, PREC_NONE},                 // TOKEN_RIGHT_BRACE
+    {NULL, NULL, PREC_NONE},                 // TOKEN_COMMA
+    {NULL, NULL, PREC_NONE},                 // TOKEN_DOT
+    {parse_unary, parse_binary, PREC_TERM},  // TOKEN_MINUS
+    {NULL, parse_binary, PREC_TERM},         // TOKEN_PLUS
+    {NULL, NULL, PREC_NONE},                 // TOKEN_SEMICOLON
+    {NULL, parse_binary, PREC_FACTOR},       // TOKEN_SLASH
+    {NULL, parse_binary, PREC_FACTOR},       // TOKEN_STAR
+    {NULL, NULL, PREC_NONE},                 // TOKEN_BANG
+    {NULL, NULL, PREC_NONE},                 // TOKEN_BANG_EQUAL
+    {NULL, NULL, PREC_NONE},                 // TOKEN_EQUAL
+    {NULL, NULL, PREC_NONE},                 // TOKEN_EQUAL_EQUAL
+    {NULL, NULL, PREC_NONE},                 // TOKEN_GREATER
+    {NULL, NULL, PREC_NONE},                 // TOKEN_GREATER_EQUAL
+    {NULL, NULL, PREC_NONE},                 // TOKEN_LESS
+    {NULL, NULL, PREC_NONE},                 // TOKEN_LESS_EQUAL
+    {NULL, NULL, PREC_NONE},                 // TOKEN_IDENTIFIER
+    {NULL, NULL, PREC_NONE},                 // TOKEN_STRING
+    {parse_number, NULL, PREC_NONE},         // TOKEN_NUMBER
+    {NULL, NULL, PREC_NONE},                 // TOKEN_AND
+    {NULL, NULL, PREC_NONE},                 // TOKEN_CLASS
+    {NULL, NULL, PREC_NONE},                 // TOKEN_ELSE
+    {NULL, NULL, PREC_NONE},                 // TOKEN_FALSE
+    {NULL, NULL, PREC_NONE},                 // TOKEN_FOR
+    {NULL, NULL, PREC_NONE},                 // TOKEN_FUN
+    {NULL, NULL, PREC_NONE},                 // TOKEN_IF
+    {NULL, NULL, PREC_NONE},                 // TOKEN_NIL
+    {NULL, NULL, PREC_NONE},                 // TOKEN_OR
+    {NULL, NULL, PREC_NONE},                 // TOKEN_PRINT
+    {NULL, NULL, PREC_NONE},                 // TOKEN_RETURN
+    {NULL, NULL, PREC_NONE},                 // TOKEN_SUPER
+    {NULL, NULL, PREC_NONE},                 // TOKEN_THIS
+    {NULL, NULL, PREC_NONE},                 // TOKEN_TRUE
+    {NULL, NULL, PREC_NONE},                 // TOKEN_VAR
+    {NULL, NULL, PREC_NONE},                 // TOKEN_WHILE
+    {NULL, NULL, PREC_NONE},                 // TOKEN_ERROR
+    {NULL, NULL, PREC_NONE},                 // TOKEN_EOF
+};
+
 static void parse_error(Parser* parser, const char* err, size_t err_len) {
     if (parser->state == PARSER_STATE_OK) {
         parser->state = PARSER_STATE_ERROR;
