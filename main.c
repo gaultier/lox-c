65a5131ef530c86504ebfff99d4e40dd8d095a89
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-01-09 13:29:06 +0100

    Add string helpers

--- main.c
+++ main.c
@@ -193,6 +193,8 @@ typedef struct {
     ObjType type;
 } Obj;
 
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
 typedef struct {
     ValueType type;
     union {
@@ -202,6 +204,12 @@ typedef struct {
     } as;
 } Value;
 
+typedef struct {
+    Obj obj;
+    size_t len;
+    char* s;
+} ObjString;
+
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
@@ -210,11 +218,18 @@ typedef struct {
 #define AS_BOOL(value) ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
 #define AS_OBJ(value) ((value).as.obj)
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->s)
 
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 #define IS_OBJ(value) ((value).type == VAL_OBJ)
+#define IS_STRING(value) value_obj_is_type(value, OBJ_STRING)
+
+static bool value_obj_is_type(Value v, ObjType type) {
+    return IS_OBJ(v) && AS_OBJ(v)->type == type;
+}
 
 static void value_print(FILE* out, Value v) {
     switch (v.type) {
