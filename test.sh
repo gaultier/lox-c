2cccc2a6aa24a8187d2c9ad5bafc0e6a9cc06dd0
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-01-21 09:38:10 +0100

    Remove trailing spaces & rename DIFFTOOL -> DIFF_TOOL

--- test.sh
+++ test.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -aue
 
-if command -v patdiff > /dev/null
-then
-    export DIFFTOOL=patdiff
-else
-    export DIFFTOOL="diff"
-fi
-
+DIFF_TOOL="${DIFF_TOOL:-diff}"
 LOXBIN="${LOXBIN:-./lox-release}"
 TMPDIR="${TMPDIR:-/tmp}"
 PARALLEL_OPTS="${PARALLEL_OPTS:- --bar -k --timeout 2}"
@@ -15,4 +9,4 @@ PARALLEL_OPTS="${PARALLEL_OPTS:- --bar -k --timeout 2}"
 find test -name '*.lox' -type f | sort | parallel $PARALLEL_OPTS " \
     awk -F '// expect: ' '/expect/{print \$2}' < {} > $TMPDIR/{/}.expected; \
     echo 5 | $LOXBIN run {} > $TMPDIR/{/}.output 2>&1; \
-    $DIFFTOOL $TMPDIR/{/}.expected $TMPDIR/{/}.output || exit 1"
+    $DIFF_TOOL $TMPDIR/{/}.expected $TMPDIR/{/}.output || exit 1"
