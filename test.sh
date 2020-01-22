e86fe8fbea4aff68b2cfc84f52fe05e7b06b41f8
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-01-22 10:55:07 +0100

    Revert "Use find -s instead of find | sort"
    This reverts commit 3e68428dd05e8878e2efe72c9e5c399e3289a469.

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ LOXBIN="${LOXBIN:-./lox-release}"
 TMPDIR="${TMPDIR:-/tmp}"
 PARALLEL_OPTS="${PARALLEL_OPTS:- --bar -k --timeout 2}"
 
-find -s test -name '*.lox' -type f | parallel $PARALLEL_OPTS " \
+find test -name '*.lox' -type f | sort | parallel $PARALLEL_OPTS " \
     awk -F '// expect: ' '/expect/{print \$2}' < {} > $TMPDIR/{/}.expected; \
     echo 5 | $LOXBIN run {} > $TMPDIR/{/}.output 2>&1; \
     $DIFF_TOOL $TMPDIR/{/}.expected $TMPDIR/{/}.output || exit 1"
