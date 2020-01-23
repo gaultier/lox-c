76f6908adc77af67809d9d23373bcfda49a972d7
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-01-23 20:16:49 +0100

    Fix clang tidy warnings incrementally

--- main.c
+++ main.c
@@ -21,9 +21,11 @@ int main(int argc, const char* argv[]) {
 
         if (strcmp(argv[1], "dump") == 0) {
             return (int)vm_interpret(source, source_len, vm_dump);
-        } else if (strcmp(argv[1], "run") == 0) {
+        }
+        if (strcmp(argv[1], "run") == 0) {
             return (int)vm_interpret(source, source_len, vm_run_bytecode);
-        } else if (strcmp(argv[1], "fmt") == 0) {
+        }
+        if (strcmp(argv[1], "fmt") == 0) {
             return (int)fmt(source, source_len);
         }
     } else {
